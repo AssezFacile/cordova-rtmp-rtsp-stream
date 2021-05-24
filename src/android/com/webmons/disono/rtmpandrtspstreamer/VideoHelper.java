8c5d400840c523b64e2d2ed4970aa2bdd730604c
Michael Jolin <jolin.michael@gmail.com>
2021-05-24 06:10:13 -0400

    Increase video bitrate

--- src/android/com/webmons/disono/rtmpandrtspstreamer/VideoHelper.java
+++ src/android/com/webmons/disono/rtmpandrtspstreamer/VideoHelper.java
@@ -10,21 +10,21 @@ package com.webmons.disono.rtmpandrtspstreamer;
 public class VideoHelper {
     public int bitrate(int width, int height) {
         if (width <= 640 && height <= 360) {
-            return 560;
+            return 1000000;
         }
 
         if (width <= 854 && height <= 480) {
-            return 1200;
+            return 2500000;
         }
 
         if (width <= 1280 && height <= 720) {
-            return 2000;
+            return 5000000;
         }
 
         if (width <= 1920 && height <= 1080) {
-            return 6000;
+            return 8000000;
         }
 
-        return 1200;
+        return 16000000;
     }
 }
